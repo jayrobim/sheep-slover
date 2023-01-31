7447eaa25deacf4de3c6648b63f502fdb12303ac
rb.wang <rb.wang@sibionics.com>
2023-01-31 15:45:01 +0800

    解析map

--- util/get.js
+++ util/get.js
@@ -1,9 +1,8 @@
 import fs from 'fs'
 import ax from './ax.js'
+import axios from "axios";
 import protobufjs from "protobufjs";
 import path from "path";
-import cc from './cocos2d-js-min'
-
 
 let url1 = 'https://cat-match.easygame2021.com/sheep/v1/game/personal_info?t='
 let url2 = 'https://cat-match.easygame2021.com/sheep/v1/game/skin/info'
@@ -29,35 +28,55 @@ let argv = process.argv[2]
 let url = urlmap[argv]
 if (url) {
     let req
-    if (argv == 'map') {
+    if (argv === 'map') {
         let str = fs.readFileSync('data/match')
         let match = JSON.parse(str)
         let name = match.data.map_md5[1] + '.map'
-        url += name
+        url = url + name
         console.log(url)
-        // console.log(axios.defaults.headers)
-        req = ax.get(url, {
-            'headers': {
-                'Host': ''
-            }
+        axios.get(url,{
+            responseType: "arraybuffer", // 表明返回服务器返回的数据类型
+        }).then((resp) => {
+            let e = new Uint8Array(resp.data);
+            protobufjs.load(path.join(process.cwd(), "data", "map.proto"), (_, root) => {
+                const GameMap = root.lookupType("map.GameMap");
+                let mapData = GameMap.decode(e.slice(21))
+                let l = {};
+                for (const i in mapData.levelData) {
+                    let nodeList =  mapData.levelData[i].node
+                    for (let j = 0; j < nodeList.length; j++) {
+                        nodeList[j].type = 0;
+                    }
+                    l[i] = nodeList;
+                }
+                let map =  Object.assign(Object.assign({}, mapData), {
+                    levelData: l
+                });
+                fs.writeFileSync('./data/' + argv, JSON.stringify(map))
+                console.log(JSON.stringify(map))
+            });
+        }).catch(e => {
+            console.log(e)
         })
-    } else if (argv == 't_join') {
-        let t = process.argv[3] || 2 //1=left, 2=right
-        req = ax.post(url, {'type': parseInt(t)})
     } else {
-        req = ax.get(url)
-    }
-    req.then((resp) => {
-        console.log(resp.data)
-        fs.writeFileSync('./data/' + argv, JSON.stringify(resp.data))
-        console.log(new Date())
-        if (argv == 'topic') {
-            fs.writeFileSync('./data/match', JSON.stringify(resp.data))
-            console.log('sync topic to match..')
+        if (argv === 't_join') {
+            let t = process.argv[3] || 2 //1=left, 2=right
+            req = ax.post(url, {'type': parseInt(t)})
+        } else {
+            req = ax.get(url)
         }
-    }).catch(e => {
-        console.log(e)
-    })
+        req.then((resp) => {
+            console.log(resp.data)
+            fs.writeFileSync('./data/' + argv, JSON.stringify(resp.data))
+            console.log(new Date())
+            if (argv === 'topic') {
+                fs.writeFileSync('./data/match', JSON.stringify(resp.data))
+                console.log('sync topic to match..')
+            }
+        }).catch(e => {
+            console.log(e)
+        })
+    }
 }
 
 
--- util/map.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-let str = fs.readFileSync('data/match')
-let match = JSON.parse(str)
-let name = match.data.map_md5[1] + '.map'
-let url = 'https://cat-match-static.easygame2021.com/maps/' + name
-console.log(url)
-
